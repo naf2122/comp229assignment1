3963f0b916c36cf7d5350ee1e2805438f1255bc0
Nafees Hasan <hasan.nafees.nafees42@gmail.com>
2021-10-24 22:34:52 -0400

    update uri to work with mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/book_store"
-    "Secret": 'SomeSecret'
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://nafees:4fkySwLXPoeaJkTo@mongodbserver.pze4f.mongodb.net/book_store?retryWrites=true&w=majority"
 }
